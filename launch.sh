c8456ecc700241b65546cd8f8d13a596779c3800
Tom de Bruijn <tom@tomdebruijn.com>
2013-10-23 08:32:26 +0200

    Replace nohup and log to own log file

--- launch.sh
+++ launch.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
 action=$1
+log=./bat.log
+
+function log {
+  echo "$1" > $log
+}
 
 if [ "$action" = "start" ]; then
-  nohup sh ./main.sh & echo $! > ./pid &
+  log "---"
+  log "Starting \"Battery alert\""
+  sh ./main.sh > $log 2>&1 & echo $! > ./pid &
 elif [ "$action" = "stop" ]; then
+  log "Stopping \"Battery alert\""
   kill -9 $(cat ./pid)
 fi
 
